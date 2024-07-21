94e6a377c30f84e2267a9edfa42839ddf430dae9
Siya Nkuna <sudosprogrammer@gmai.com>
2024-07-21 15:03:52 +0200

    update env variables

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
-const port = 5000;
+
+dotenv.config();
+
+const port = process.env.PORT;
 
 app.get('/', (req, res,) => {
   res.send('Hello, TypeScript with Express!',);
