db29f62bbb25aa438500b1b008a414caffca33ed
Walter Nkosinathi Magagula <walter.magagula@bbd.co.za>
2024-07-21 20:48:34 +0200

    infrastructure initial commit

---
+++ SDESInfrastructure/lib/sdes_infrastructure-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class SdesInfrastructureStack extends cdk.Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // The code that defines your stack goes here
+  }
+}
