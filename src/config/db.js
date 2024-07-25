f796a620c46257e39eb4eb5a19fec9b48fa6dd1b
Walter Nkosinathi Magagula <walter.magagula@bbd.co.za>
2024-07-25 22:48:18 +0200

    fix for psql on ec2

--- src/config/db.js
+++ src/config/db.js
@@ -4,27 +4,27 @@ require('dotenv').config({ override: true }); // Load environment variables from
 let client;
 let debug = process.env.DEBUG;
 
-if (debug){
-  client = new Client({
-    user: process.env.LOCAL_USER,
-    password: process.env.LOCAL_PASSWORD,
-    host: process.env.LOCAL_ENDPOINT,
-    port: process.env.LOCAL_PORT,
-    database: process.env.LOCAL_DATABASE="spatial_db"
-    ,
-  });
-}else {
-  client = new Client({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.ENDPOINT,
-    port: process.env.DB_PORT,
-    database: process.env.DATABASE,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-}
+// if (debug){
+//   client = new Client({
+//     user: process.env.LOCAL_USER,
+//     password: process.env.LOCAL_PASSWORD,
+//     host: process.env.LOCAL_ENDPOINT,
+//     port: process.env.LOCAL_PORT,
+//     database: process.env.LOCAL_DATABASE="spatial_db"
+//     ,
+//   });
+// }else {
+client = new Client({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.ENDPOINT,
+  port: process.env.PORT,
+  database: process.env.DATABASE,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+// }
 
 client.connect(err => {
   if (err) {
